ffe214eebdf2ffd4f73ad8335fe59ee04831a4fd
jonhopper-dataengineers <jon@dataengineers.co.nz>
2022-08-24 17:41:20 +1200

    updated code for stored proc testing

--- macros/atomic-unit-tests/snowflake_create_stored_procedure_statement.sql
+++ macros/atomic-unit-tests/snowflake_create_stored_procedure_statement.sql
@@ -1,13 +1,14 @@
-{%- macro snowflake_create_stored_procedure_statement(relation, return_type, sql) -%}
+@@ -0,0 +1,13 @@
+{%- macro snowflake_create_stored_procedure_statement(relation, preferred_language, return_type, sql) -%}
 
     {{ log("Creating Stored Procedure " ~ relation) }}   
 CREATE OR REPLACE PROCEDURE {{ relation.include(database=(not temporary), schema=(not temporary)) }}()
-returns {{ return_type }}
-language sql
+RETURNS {{ return_type }}
+LANGUAGE {{ preferred_language }}
 AS
 $$
     {{ sql }}
 $$
 ;
 
-{%- endmacro -%}
+{%- endmacro -%} 
