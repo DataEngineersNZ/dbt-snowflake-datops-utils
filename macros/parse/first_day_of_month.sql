fcfa7e0348faf3c57844af1eb52389c875e0e222
jonhopper-dataengineers <jon@dataengineers.co.nz>
2023-01-18 11:03:13 +1300

    updated macro names to be the same as the docs

--- macros/parse/first_day_of_month.sql
+++ macros/parse/first_day_of_month.sql
@@ -1,3 +1,3 @@
-{%- macro first_day_month(s_year, s_month, month_format) -%}
+{%- macro first_day_of_month(s_year, s_month, month_format) -%}
     TO_DATE(CONCAT({{ extract_year }}, '-', {{ extract_month }}, '-01'), 'YYYY-{{ month_format }}-DD')
 {%- endmacro -%}
--- macros/parse/last_day_of_month.sql
+++ macros/parse/last_day_of_month.sql
@@ -1,3 +1,3 @@
-{%- macro last_day_month(s_year, s_month, month_format) -%}
+{%- macro last_day_of_month(s_year, s_month, month_format) -%}
     LAST_DAY(TO_DATE(CONCAT({{ extract_year }}, '-', {{ extract_month }}, '-01'), 'YYYY-{{ month_format }}-DD'), MONTH)
 {%- endmacro -%}
