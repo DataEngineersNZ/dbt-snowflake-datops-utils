ea6fc25afad6036e5a40f03306386c8b5d63fed9
jonhopper-dataengineers <jon@dataengineers.co.nz>
2023-01-26 18:59:32 +1300

    fixed up unit test helpers

---
+++ macros/atomic-unit-tests/get_value_or_null.sql
@@ -0,0 +1,9 @@
+{%- macro get_value_or_null(value, inc_quotes) -%}
+    {%- if value == "null" -%}
+        {{ value }}
+    {%- elif inc_quotes -%}
+        '{{ value }}'
+    {%- else -%}
+        {{ value }}
+    {%- endif -%}
+{%- endmacro -%}
