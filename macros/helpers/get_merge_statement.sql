03e50cf47af8d1d22431d68e4a5fe59148b30561
jonhopper-dataengineers <jon@dataengineers.co.nz>
2023-12-20 13:03:40 +1300

    fixed up merge statement

--- macros/helpers/get_merge_statement.sql
+++ macros/helpers/get_merge_statement.sql
@@ -3,5 +3,5 @@
 {%- set source_relation = api.Relation.create(identifier=source.identifier, schema=source.schema, database=database) -%}
 {%- set destination_relation = adapter.get_relation( identifier=destination_table, schema=destination_schema, database=database) -%}
 {%- set dest_columns = adapter.get_columns_in_relation(destination_relation) -%}
-{{ get_merge_sql(destination_relation, source_relation, unique_key, dest_columns, predicates) }}
+{{ get_merge_sql(destination_relation, source_relation, unique_key, dest_columns, predicates)  | replace("begin;", "") | replace("commit;", "") }}
 {%- endmacro -%}
