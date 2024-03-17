6aa13da3d35f8f88534c6b8b232ea562b6f1b796
jonhopper-dataengineers <jon@dataengineers.co.nz>
2024-03-17 15:51:57 +1300

    fixed up formatting issues

--- macros/grants/grant_database_usage.sql
+++ macros/grants/grant_database_usage.sql
@@ -9,13 +9,13 @@
             {% for row in results %}
                 {% if row.privilege == "USAGE" and row.granted_to in ["ROLE"] %}
                     {% if row.grantee_name not in grant_roles %}
-                        {{ statements.append("revoke usage on database "  ~ target.database | lower  ~ | " from role " ~ row.grantee_name | lower  ~ ";") }}
+                        {{ statements.append("revoke usage on database "  ~ target.database | lower  ~ " from role " ~ row.grantee_name | lower  ~ ";") }}
                     {% else %}
                         {{ existing_roles.append(row.grantee_name) }}
                     {% endif %}
                 {% elif row.privilege == "USAGE" and row.granted_to in ["SHARE"] %}
                     {% if row.grantee_name not in grant_shares %}
-                        {{ statements.append("revoke usage on database "  ~ target.database | lower  ~ | " from share " ~ row.grantee_name | lower  ~ ";") }}
+                        {{ statements.append("revoke usage on database "  ~ target.database | lower  ~ " from share " ~ row.grantee_name | lower  ~ ";") }}
                     {% else %}
                         {{ existing_shares.append(row.grantee_name) }}
                     {% endif %}
@@ -23,12 +23,12 @@
             {% endfor %}
             {% for role in grant_roles %}
                 {% if role not in existing_roles %}
-                    {{ statements.append("grant usage on database "  ~ target.database | lower  ~ | " to role " ~ role | lower  ~ ";") }}
+                    {{ statements.append("grant usage on database "  ~ target.database | lower  ~ " to role " ~ role | lower  ~ ";") }}
                 {%endif%}
             {% endfor %}
             {% for share in grant_shares %}
                 {% if share not in existing_shares %}
-                    {{ statements.append("grant usage on database "  ~ target.database | lower  ~ | " to share " ~ share | lower  ~ ";") }}
+                    {{ statements.append("grant usage on database "  ~ target.database | lower  ~ " to share " ~ share | lower  ~ ";") }}
                 {%endif%}
             {% endfor %}
             {% for statement in statements %}
